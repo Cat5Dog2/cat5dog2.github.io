ec88c7c87b56e6353e4a6260e674012a0447e6ab
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2026-06-28 18:35:00 +0900

    feat: add mockup showcase component to handmade item project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   site: 'https://cat5dog2.github.io',
+  integrations: [mdx()],
 });
